463b0034970e05cd53a37e59346b113b7ad11915
Dirk Jagdmann <doj@cubic.org>
2020-03-05 18:37:36 -0800

    clean up auto* generated files

--- make.sh
+++ make.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-automake
-autoconf
+
+if [ "$1" = clean ] ; then
+    make distclean
+    rm -rf autom4te.cache configure aclocal.m4
+    find . -name Makefile.in -delete
+    find . -name '*~' -delete
+    find . -name .deps -type d | xargs rm -r
+    rm unix/compile unix/config.guess unix/config.sub unix/install-sh unix/missing unix/depcomp unix/ltmain.sh
+    exit
+fi
+
+set -e
+autoreconf --install --symlink
 ./configure "$@"
 make
